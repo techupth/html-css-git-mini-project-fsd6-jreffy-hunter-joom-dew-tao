b86db3cfce3bc5be4c25e6b4b175176708839d22
Jeffiros <krittiwat.c@gmail.com>
2024-05-15 11:24:09 +0700

    fix:responsive design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  theme: {
+    extend: {  
+      colors: {
+          'intro-background' : '#FFFBF4',
+          '40kIcon' : '#51C8FF'
+      },
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans-serif'],
+        'rubik': ['rubik', 'sans-serif']
+      }
+    }
+  },
+};
